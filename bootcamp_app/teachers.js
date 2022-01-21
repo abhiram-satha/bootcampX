209d7ecced7d53599956545dcfb5c9bdeb09ef7f
Abhiram <abhiram.satha@gmail.com>
2022-01-21 00:46:04 +0000

    Adding query string for added security

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher ASC;
-`)
+`;
+
+const limit = [`${process.argv[2] || 'JUL02'}`];
+
+pool.query(queryString, limit)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`)
